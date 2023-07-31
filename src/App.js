7ce3ef61ce6e122e6c05cfd481fec79ea6e81088
AlaneGadelha <112704003+AlaneGadelha@users.noreply.github.com>
2023-07-31 12:23:50 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+// import logo from './logo.svg';
+// import harry from './channels4_profile.jpg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p className='p-tomato'>Tomato</p>
+//         <p className='p-orange'>Orange</p>
+//         <p className='p-dodgerBlue'>DodgerBlue</p>
+//         <p className='p-medium'>MediumSeaGreen</p>
+//         <p className='p-gray'>Gray</p>
+//         <p className='p-slate'>SlateBlue</p>
+//         <p className='p-violet'>Violet</p>
+//         <p className='p-ligth'>LigthGray</p>
+
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import logo from './logo.svg';
+// import harry from './channels4_profile.jpg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+      
+//         <table>
+//        <tr className='linha'>
+//         <td className='marca'>Marca</td>
+//         <td className='veiculo'>Veiculo</td>
+//       </tr>
+//       <tr>
+//         <td className='chev'>Chevrolet</td>
+//         <td className='chev'>Sonic</td>
+//       </tr>
+//       <tr>
+//         <td className='chevro'>Chevrolet</td>
+//         <td className='chevro'>Cruze</td>
+//       </tr>
+//       <tr>
+//         <td className='land'>Land Rover</td>
+//         <td className='land'>Evoque</td>
+//       </tr>
+//       <tr>
+//         <td className='volk'>Volkwagem</td>
+//         <td className='volk'>Jeta</td>
+//       </tr>
+//       <tr>
+//         <td className='ford'> Ford</td>
+//         <td className='ford'> Focus</td>
+//       </tr>
+//       </table>
+       
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
